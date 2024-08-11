ba40a3f8afda2105b43bf7cf8b48f986d1e7037a
Vincent Kok <vincentkok@live.nl>
2024-08-11 12:24:22 +0200

    Add terraform to create cosmosdb account

---
+++ infrastructure/cosmos.tf
@@ -0,0 +1,16 @@
+﻿resource "azurerm_cosmosdb_account" "db" {
+  name                = "cosdb-telemetry-${var.environment}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.telemetry_rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  free_tier_enabled   = true
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+  geo_location {
+    failover_priority = 0
+    location          = var.location
+  }
+}
---
+++ infrastructure/main.tf
@@ -0,0 +1,4 @@
+﻿resource "azurerm_resource_group" "telemetry_rg" {
+  location = var.location
+  name     = "rg-telemetry-app-${var.environment}"
+}
